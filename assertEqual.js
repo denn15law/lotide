ebd3e9f708408aed0dad4f2644b481a42ce63ff7
Dennis Law <denn15_law@live.ca>
2021-10-25 18:58:50 +0000

    added template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✅ ✅ ✅ Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log('🚫 🚫 🚫 Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`🚫 🚫 🚫 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
