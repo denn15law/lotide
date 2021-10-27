28822d5013fe59bc1c0decd977a18c24fc8bb470
Dennis Law <denn15_law@live.ca>
2021-10-27 03:06:45 +0000

    create without.js

---
+++ without.js
@@ -0,0 +1,63 @@
+const eqArrays = function(firstArray, secondArray) {
+  
+  let output = false;
+
+  if (firstArray.length !== secondArray.length) {
+    return output;
+  } else {
+    for (let i in firstArray) {
+      if (firstArray[i] === secondArray[i]) {
+        output = true;
+      } else {
+        output = false;
+        break;
+      }
+    }
+  }
+
+  return output;
+
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log("✅ ✅ ✅ These arrays are equal");
+  } else {
+    console.log("🚫 🚫 🚫These arrays are not equal");
+  }
+};
+
+const without = function (source, itemsToRemove){
+  let output = []
+  let keep
+
+  for (let itemSource of source){
+    keep = true;
+    for(let itemRemove of itemsToRemove){
+
+      if(itemSource !== itemRemove){
+        keep = false
+      }
+
+      if(keep){
+        output.push(itemSource)
+      }
+    }
+  }
+  console.log(output)
+  return output
+}
+without(["1", "2", "3"], [1, 2, "3"]) //  => ["1", "2"]
+
+
+
+
+
+
+
+
+
+
+// const words = ["hello", 'world', 'lighthouse']
+// without(words, ['lighthouse'])
+// assertArraysEqual(words, ["hello", 'world', 'lighthouse'])
