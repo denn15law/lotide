3a2175ee028f551d1340b5e915ceb91f733cd960
Dennis Law <denn15_law@live.ca>
2021-10-27 00:21:38 +0000

    add working solution for eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 🚫 🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  
+  let output = false;
+
+  if (firstArray.length !== secondArray.length) {
+    return output;
+  } else {
+    for (let i in firstArray) {
+      if (firstArray[i] === secondArray[i]) {
+        output = true;
+      } else {
+        output = false;
+        break;
+      }
+    }
+  }
+
+  return output;
+
+};
+
+assertEqual(eqArrays([2, 3], [1, 2, 3]), true);
