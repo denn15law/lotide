d751ad76c15f551586168fca2d9dc5f401873e9c
Dennis Law <denn15_law@live.ca>
2021-10-27 17:43:59 +0000

    add working solution for even and odd conditons

--- middle.js
+++ middle.js
@@ -30,17 +30,42 @@ const assertArraysEqual = function(firstArray, secondArray) {
 const middle = function (arr){
   
   let output = []
+  let oddIndex
+  let evenIndex = []
 
   if (arr.length === 1 || arr.length === 2){
     console.log(output)
+    return output
   }else {
     if (arr.length % 2 === 0){
-      console.log('even')
+      // console.log('even')
+      evenIndex = [arr.length/2 - 1, arr.length/2]
+      console.log(evenIndex)
+      for (let num of evenIndex){
+        output.push(arr[num])
+      }
+      console.log(output)
     }else{
-      console.log('odd')
+      // console.log('odd')
+      oddIndex = Math.floor(arr.length / 2)
+      // console.log(oddIndex)
+      output.push(arr[oddIndex])
+      // console.log(output)
     }
   }
-  
+  return output
 }
 
-middle([1, 2])
+// middle([1]) // => []
+// middle([1, 2]) // => []
+//odd
+// middle([1, 2, 3]) // => [2]
+// middle([1, 2, 3, 4, 5]) // => [3]
+//even
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+
+
+//test assertions
+
+assertArraysEqual(middle([1,2,3,4]),[2,3])
