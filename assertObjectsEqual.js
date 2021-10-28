1f34dd501e24e90e50459e6a99c660e836f8d9e9
Dennis Law <denn15_law@live.ca>
2021-10-28 01:35:59 +0000

    add inspect util to assertObjectEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -47,11 +47,12 @@ const eqObjects = function (object1, object2){
 
 const assertObjectsEqual = function (actual, expected) {
   
+  const inspect = require('util').inspect;
   
   if(eqObjects(actual, expected)){
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
   }else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`)
   }
 }
 
