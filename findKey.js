21736072e79e06844440ded306b9bfd0c9d0c3c2
Dennis Law <denn15_law@live.ca>
2021-10-29 01:58:43 +0000

    implement working solution for assertion tests

--- findKey.js
+++ findKey.js
@@ -12,7 +12,15 @@ const findKey = (obj, callback) => {
   let keys = Object.keys(obj)
   
   for (let key of keys){
-    console.log(obj[key].stars)
+    
+    // console.log(key)
+    // console.log(obj[key])
+    // console.log(obj[key].stars)
+    // console.log(callback(obj[key]))
+
+    if(callback(obj[key])){
+      return key
+    }
   }
 }
 
@@ -24,3 +32,12 @@ findKey({
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2) // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma')
