05a534ba959b062d3c9743a9b4fac4712e5788b2
Dennis Law <denn15_law@live.ca>
2021-11-02 01:18:37 +0000

    add findKeyTest and refractor for mocha

--- findKey.js
+++ findKey.js
@@ -1,43 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚫 🚫 🚫 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const findKey = (obj, callback) => {
   
   let keys = Object.keys(obj)
   
   for (let key of keys){
-    
-    // console.log(key)
-    // console.log(obj[key])
-    // console.log(obj[key].stars)
-    // console.log(callback(obj[key]))
-
     if(callback(obj[key])){
-      return key
+      return key;
     }
   }
 }
 
-findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), 'noma')
+module.exports = findKey;
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+
 
 module.exports = {
   head: head,
